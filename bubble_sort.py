410418d982d77726337447aff38e42e201fd5e27
Chinnu Ajith <chin3nu@gmail.com>
2020-06-03 21:19:45 -0400

    bubble_sort

---
+++ bubble_sort.py
@@ -0,0 +1,12 @@
+""" 
+BUBBLE SORT
+ """
+
+def bs(a):             
+    b=len(a)-1       
+                             
+    for x in range(b):
+        for y in range(b-x):
+            if a[y]>a[y+1]:
+                a[y],a[y+1]=a[y+1],a[y]
+    return a
